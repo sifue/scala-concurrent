9429a0cdfd443e7ac35d48fc149a423a152fb3f3
Soichiro-Yoshimura <yoshimura@soichiro.org>
2017-06-05 21:17:53 +0900

    sychronizedブロックで対応

--- src/main/scala/MemoryVisibility.scala
+++ src/main/scala/MemoryVisibility.scala
@@ -1,14 +1,18 @@
 object MemoryVisibility extends App {
-  @volatile var number = 0
-  @volatile var ready = false
+  var number = 0
+  var ready = false
+  private[this] def getNumber: Int = synchronized { number }
+  private[this] def getReady: Boolean = synchronized { ready }
 
   new Thread(() => {
-    while (!ready) {
+    while (!getReady) {
       Thread.`yield`()
     }
-    println(number)
+    println(getNumber)
   }).start()
 
-  number = 2525
-  ready = true
+  synchronized {
+    number = 2525
+    ready = true
+  }
 }
