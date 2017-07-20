913b48de6b3a2aa8b88f857b0093d799e9374869
Soichiro-Yoshimura <yoshimura@soichiro.org>
2017-07-20 15:20:34 +0900

    Akkaとアクターモデルの実装を追加

---
+++ src/main/scala/ActorStudy.scala
@@ -0,0 +1,28 @@
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.event.Logging
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class MyActor extends Actor {
+  val log = Logging(context.system, this)
+
+  def receive = {
+    case "test" => log.info("received test")
+    case _ => log.info("received unknown message")
+  }
+}
+
+object ActorStudy extends App {
+  val system = ActorSystem("actorStudy")
+
+  val myActor = system.actorOf(Props[MyActor], "myActor")
+
+  myActor ! "test"
+
+  myActor.tell("text", Actor.noSender)
+
+  myActor ! "hoge"
+
+  Await.ready(system.terminate(), 5000.millisecond)
+}
